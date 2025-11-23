07b2069542fe35bd857afcb75ec3e10632b89690
olafnub <googlhoogle@gmail.com>
2025-11-23 14:33:00 -0600

    Have connect solana working

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { SendSOLToRandomAddress } from './send-sol';
 
 export default function Home() {
   return (
     <div>
       <p>Yo</p>
+      <SendSOLToRandomAddress />
     </div>
   );
 }
--- my-app/src/components/solana/solana-provider.tsx
+++ my-app/src/components/solana/solana-provider.tsx
@@ -2,44 +2,43 @@
 // SolanaProvider from https://solana.com/developers/cookbook/wallets/connect-wallet-react
 // Layout from wallet-ui
 'use client';
+import dynamic from 'next/dynamic';
 import { ReactNode, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { 
     WalletModalProvider,
-    WalletMultiButton,
     WalletDisconnectButton
  } from '@solana/wallet-adapter-react-ui';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-// import { clusterApi } from '@solana/kits';
+import { clusterApiUrl } from '@solana/web3.js';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
+
+// https://solana.stackexchange.com/questions/4304/error-hydration-failed-because-the-initial-ui-does-not-match-what-was-rendered
+const WalletMultiButtonDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
+
 
 export function SolanaProvider( {children}: {children: ReactNode}) {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
     const network = WalletAdapterNetwork.Devnet;
 
-    // const endpoint = useMemo(() => clusterApi(network, [network]));
-    const endpoint = "https://api.devnet.solana.com";
-
-    const wallets = useMemo(
-        () => [
-            new UnsafeBurnerWalletAdapter(),
-        ],
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [network]
-    )
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
+            <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
-                    <WalletMultiButton />
-                    <WalletDisconnectButton />
+                    <WalletMultiButtonDynamic />
+                    {/* <WalletDisconnectButton /> */}
                     {children}
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
     )
-
 }
+
+// future https://github.com/anza-xyz/kit/blob/main/examples/react-app/src/components/WalletAccountIcon.tsx
