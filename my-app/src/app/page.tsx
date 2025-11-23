6558a9ad58d60b8ff76d71b0088572a7fa31e5f0
olafnub <googlhoogle@gmail.com>
2025-11-23 14:33:22 -0600

    Remove unused component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { SendSOLToRandomAddress } from './send-sol';
-
 export default function Home() {
   return (
     <div>
       <p>Yo</p>
-      <SendSOLToRandomAddress />
     </div>
   );
 }
